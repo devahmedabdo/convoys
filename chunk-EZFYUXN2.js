6f7a82edebf1e2f5c495c1812668b0277ce6d50d
Ahmed Abdo <devahmedabdo@gmail.com>
2026-08-28 00:38:50 +0300

    awd

---
+++ chunk-EZFYUXN2.js
@@ -0,0 +1,1 @@
+import{V as n}from"./chunk-GMMA7UKE.js";var o=new n("SA_GENERAL_I18N");export{o as a};
--- chunk-YA4FI5UY.js
+++
@@ -1,1 +0,0 @@
-import{V as n}from"./chunk-IEURG37R.js";var o=new n("SA_GENERAL_I18N");export{o as a};
