5d169998433b5097f7c88c336edfe5d7fbe73b78
Ahmed Abdo <devahmedabdo@gmail.com>
2026-08-27 23:59:29 +0300

    new convoy system

---
+++ chunk-LZKX2KLW.js
@@ -0,0 +1,1 @@
+import{V as e}from"./chunk-IEURG37R.js";var S=new e("SA_SELECT_I18N");export{S as a};
---
+++ chunk-YA4FI5UY.js
@@ -0,0 +1,1 @@
+import{V as n}from"./chunk-IEURG37R.js";var o=new n("SA_GENERAL_I18N");export{o as a};
